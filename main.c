34e9bff5fd1899a799d714c71546415480a2ff7c
zhanghuaimin666 <3416214532@qq.com>
2025-09-28 16:53:39 +0800

    Initial commit

--- main.c
+++ main.c
@@ -707,7 +707,8 @@ int getHead(Queue *Q,ElemType *e) {
     return 1;
 }*/
 
-typedef struct {
+//循环队列
+/*typedef struct {
     ElemType *data;
     int front;
     int rear;
@@ -748,6 +749,57 @@ int getHead(Queue *Q,ElemType *e) {
     }
     *e=Q->data[Q->front];
     return 1;
+}*/
+
+//队列的链式结构
+typedef struct QueueNode {
+    ElemType data;
+    struct QueueNode *next;
+} QueueNode;
+
+typedef struct {
+    QueueNode *front, *rear;
+} Queue;
+
+Queue *initQueue() {
+    Queue *q = (Queue *) malloc(sizeof(Queue));
+    QueueNode *node = (QueueNode *) malloc(sizeof(QueueNode));
+    node->data = 0;
+    node->next = NULL;
+    q->front = node;
+    q->rear = node;
+    return q;
+}
+
+int isEmpty(Queue *q) {
+    return q->front == q->rear;
+}
+
+void equeue(Queue *q, ElemType e) {
+    QueueNode *node = (QueueNode *) malloc(sizeof(QueueNode));
+    node->data = e;
+    node->next = NULL;
+    q->rear->next = node;
+    q->rear = node;
+}
+
+int dequeue(Queue *q, ElemType *e) {
+    QueueNode *node = q->front->next;
+    *e = node->data;
+    q->front->next = node->next;
+    if (q->rear == node) {
+        q->rear = q->front;
+    }
+    free(node);
+    return 1;
+}
+
+ElemType getFront(Queue *q) {
+    if (isEmpty(q)) {
+        printf("空的\n");
+        return 0;
+    }
+    return q->front->next->data;
 }
 
 int main(void) {
@@ -1188,7 +1240,7 @@ else {
     getHead(&q,&e);
     printf("%d\n",e);*/
 
-    Queue *q=initQueue();
+    /*Queue *q=initQueue();
 
     equeue(q,10);
     equeue(q,20);
@@ -1203,7 +1255,20 @@ else {
     printf("%d\n",e);
 
     getHead(q,&e);
-    printf("%d\n",e);
+    printf("%d\n",e);*/
+
+    /*Queue *q=initQueue();
+    equeue(q,10);
+    equeue(q,20);
+    equeue(q,30);
+    equeue(q,40);
+    equeue(q,50);
+    ElemType e;
+    dequeue(q,&e);
+    printf("出队%d\n",e);
+    dequeue(q,&e);
+    printf("出队%d\n",e);
+    printf("%d\n",getFront(q));*/
 
 
     return 0;
